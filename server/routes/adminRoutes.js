9bb34d4eaf0af70b3d1d17fb04a431b503ac737a
tatiana <tatiana.turin@gmail.com>
2025-08-17 13:06:13 -0400

    deploy to package.json

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const {exportAssignmentsCsv, addSampleData} = require('../controllers/adminController');
+const {exportAssignmentsCsv} = require('../controllers/adminController');
 
 //ROOT LOCALHOST:5000/api/admin
 
 router.get('/exportAssignment',exportAssignmentsCsv);
-router.get('/add-sample-data', addSampleData);
 
 
 
