4e0ed472d6bf559caa1bac314ae9e231599108c0
tatiana <tatiana.turin@gmail.com>
2025-08-19 20:39:15 -0400

    added with credentials frontend login bar

--- client/src/components/LoginBar.jsx
+++ client/src/components/LoginBar.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+axios.defaults.withCredentials = true;
 
 const LoginBar = ({onLogin}) =>{
     const [email, setEmail] = useState('');
